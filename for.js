66d83793edc14874ce5aac62e39e3a31c62113f3
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-07-31 16:30:31 +0600

    first commit

---
+++ for.js
@@ -0,0 +1,15 @@
+const numbers = [12, 34, 65, 71, 89,97, 45]
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+    
+}
+const fruits=['orange', 'banana','apple', 'mango','watermelon']
+console.log(fruits);
+for (const fruit of fruits){
+    console.log(fruit);
+    console.log('I will want to eat');
+    
+    
+}
---
+++ while.js
@@ -0,0 +1,11 @@
+let num = 0
+while (num<5) {
+    console.log('Hello');
+    num= num + 1
+    
+}
+let num1 =0;
+while (num1<10) {
+    console.log('fruit');
+    num1++
+}
