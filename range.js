b1e960c6e7bbd6c9119e05bd6e0492765fae90e4
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-07-31 17:00:32 +0600

    while loop are added

---
+++ range.js
@@ -0,0 +1,15 @@
+let num =1;
+while (num<10) {
+    console.log(num);
+   num++ 
+}
+let num1 = 1;
+while (num1<11) {
+    console.log(num1);
+    num1++
+}
+let num2 = 11;
+while (num2<=20) {
+    console.log(num2);
+    num2= num2 + 1
+}
---
+++ sum.js
@@ -0,0 +1,18 @@
+let num = 1;
+let sum = 0;
+while (num<=10) {
+    console.log(num);
+    sum = sum + num
+    console.log('sum:', sum);
+    num++
+    
+}
+let num1 = 11;
+let sum1 = 0;
+while (num1<=30) {
+    sum1= sum1 + num1
+    console.log('sum1', sum1);
+    num1++
+    
+    
+}
