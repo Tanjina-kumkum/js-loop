a33ab3511746dac39c03cfe1b18f3203086ee33c
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-08-01 09:16:49 +0600

    decrement while and for loop are add

---
+++ drecremental.js
@@ -0,0 +1,8 @@
+for(let i = 10; i>=0 ; i--){
+    console.log(i);
+    
+}
+for(let x=20; x>=11; x--){
+    console.log(x);
+    
+}
---
+++ for.sum.js
@@ -0,0 +1,12 @@
+// 
+let sum =0;
+for(let i = 11; i<=20; i++){
+    sum= sum + i
+}
+console.log('sum of numbers from 11 to 20', sum);
+let num= 0;
+for(let x= 1; x< 20 ; x++){
+    num = num + x;
+    console.log( num);
+    
+}
